f636c34331f7acce2256d7ff229a914deb9d5b53
Prashant Wason <pwason@uber.com>
2023-05-18 09:22:50 -0700

    [HUDI-53] Implementation of record_index - a HUDI index based on the metadata table.
    Added unit test for failed bootstrap case.
    
    Raise an exception if location of any existing record is detected while updating record index.
    
    Log the records when duplicate keys are found while writing in HFileWriter.
    
    When creating HoodieTableMetadata check that metadata table is already initialized.
    
    Fallback index for record index made into a STATIC variable.
    
    Fix timeline instant and check for deltacommits in mdt timeline
    
    Comment no base file validation
    
    Add record_index to valid index types
    
    Parallelize getRecordsByKey()
    Changed the function return to Map<String. HoodieRecord>. There is no need to return non-existing keys as Option since we anyways check for Option.present() in the return value.
    
    When millions of keys are being checked (record index) then creating millions of Option instances and the millions of redundant checks can be avoided.
    
    hudi-cli command to delete MDT and record-index
    
    Fallback index type should be global like record index.
    
    More unit tests for record index
    
    [UBER] Fix instant time condition check for record_index payload
    
    Add more tests on indexes
    
    [UBER] Include config to block duplicates while writing to HFiles
    
    [UBER] Emit metrics for record_index and other metadata partitions bootstrap failures
    
    [UBER] Reduce hdfs file list calls in record_index taglocation method
    
    [UBER] Fix NPE when updating record_index for clustering
    
    [UBER] Include unit tests on record index to validate out of order commits and clustering commits

--- hudi-client/hudi-spark-client/src/test/java/org/apache/hudi/table/TestCleaner.java
+++ hudi-client/hudi-spark-client/src/test/java/org/apache/hudi/table/TestCleaner.java
@@ -596,7 +596,7 @@ public class TestCleaner extends HoodieClientTestBase {
           }
         })
     );
-    metadataWriter.update(commitMetadata, "00000000000001");
+    metadataWriter.update(commitMetadata, context.emptyHoodieData(), "00000000000001");
     metaClient.getActiveTimeline().saveAsComplete(
         new HoodieInstant(State.INFLIGHT, HoodieTimeline.COMMIT_ACTION, "00000000000001"),
         Option.of(commitMetadata.toJsonString().getBytes(StandardCharsets.UTF_8)));
@@ -1051,7 +1051,7 @@ public class TestCleaner extends HoodieClientTestBase {
           }
         })
     );
-    metadataWriter.update(commitMetadata, "00000000000001");
+    metadataWriter.update(commitMetadata, context.emptyHoodieData(), "00000000000001");
     metaClient.getActiveTimeline().saveAsComplete(
         new HoodieInstant(State.INFLIGHT, HoodieTimeline.COMMIT_ACTION, "00000000000001"),
         Option.of(commitMetadata.toJsonString().getBytes(StandardCharsets.UTF_8)));
@@ -1186,7 +1186,7 @@ public class TestCleaner extends HoodieClientTestBase {
       }
     });
     metadataWriter.performTableServices(Option.of(instantTime));
-    metadataWriter.update(commitMeta, instantTime);
+    metadataWriter.update(commitMeta, context.emptyHoodieData(), instantTime);
     metaClient.getActiveTimeline().saveAsComplete(
         new HoodieInstant(State.INFLIGHT, HoodieTimeline.COMMIT_ACTION, instantTime),
         Option.of(commitMeta.toJsonString().getBytes(StandardCharsets.UTF_8)));
