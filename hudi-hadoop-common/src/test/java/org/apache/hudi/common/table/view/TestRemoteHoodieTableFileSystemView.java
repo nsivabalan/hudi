913a97921aa0457a431256f4a7a67b28fcff48d9
Lin Liu <141371752+linliu-code@users.noreply.github.com>
2025-08-29 19:59:42 -0700

    [ENG-22138][INTERNAL] Close remote fsView and heartbeat clients properly (#1538)
    Co-authored-by: Vinish Reddy <vinishreddypannala@infinilake.com>

--- hudi-hadoop-common/src/test/java/org/apache/hudi/common/table/view/TestRemoteHoodieTableFileSystemView.java
+++ hudi-hadoop-common/src/test/java/org/apache/hudi/common/table/view/TestRemoteHoodieTableFileSystemView.java
@@ -26,10 +26,12 @@ import org.apache.hudi.common.table.timeline.HoodieInstant;
 import org.apache.hudi.common.table.timeline.HoodieTimeline;
 import org.apache.hudi.common.table.timeline.dto.TimelineDTO;
 import org.apache.hudi.common.table.timeline.versioning.v2.InstantComparatorV2;
+import org.apache.hudi.exception.HoodieException;
 import org.apache.hudi.exception.HoodieRemoteException;
 import org.apache.hudi.timeline.TimelineServiceClient;
 import org.apache.hudi.timeline.TimelineServiceClientBase;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import org.junit.jupiter.api.Test;
@@ -47,10 +49,14 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import static org.apache.hudi.common.table.view.RemoteHoodieTableFileSystemView.CLOSE_TABLE_URL;
 import static org.apache.hudi.common.table.view.RemoteHoodieTableFileSystemView.GET_TIMELINE_HASH_URL;
 import static org.apache.hudi.common.table.view.RemoteHoodieTableFileSystemView.INIT_TIMELINE_URL;
+import static org.apache.hudi.common.table.view.RemoteHoodieTableFileSystemView.REFRESH_TABLE_URL;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -132,5 +138,79 @@ class TestRemoteHoodieTableFileSystemView extends TestHoodieTableFileSystemView 
     TimelineServiceClientBase.Request request = argCaptor.getValue();
     assertFalse(request.getBody().isEmpty());
     assertEquals(OBJECT_MAPPER.writeValueAsString(partitionPath), request.getBody());
+  }
+
+  @ParameterizedTest
+  @ValueSource(booleans = {true, false})
+  void syncRemoteView(boolean enableRemoteInitTimeline) throws IOException {
+    // Write data to a single partition.
+    String partitionPath = "partition1";
+    Paths.get(basePath, partitionPath).toFile().mkdirs();
+    String fileId = UUID.randomUUID().toString();
+    String instantTime1 = "1";
+    String fileName1 = FSUtils.makeBaseFileName(instantTime1,"1-0-1", fileId, HoodieTableConfig.BASE_FILE_FORMAT.defaultValue().getFileExtension());
+    Paths.get(basePath, partitionPath, fileName1).toFile().createNewFile();
+    HoodieActiveTimeline commitTimeline = metaClient.getActiveTimeline();
+    HoodieInstant instant1 = new HoodieInstant(HoodieInstant.State.INFLIGHT, HoodieTimeline.COMMIT_ACTION, instantTime1, InstantComparatorV2.REQUESTED_TIME_BASED_COMPARATOR);
+    saveAsComplete(commitTimeline, instant1, new HoodieCommitMetadata());
+    metaClient.reloadActiveTimeline();
+
+    FileSystemViewStorageConfig config = FileSystemViewStorageConfig.newBuilder()
+        .withRemoteInitTimeline(enableRemoteInitTimeline)
+        .build();
+
+    // Mock calls made during initialization and sync while capturing request args for validation
+    ArgumentCaptor<TimelineServiceClientBase.Request> captor = ArgumentCaptor.forClass(TimelineServiceClientBase.Request.class);
+    if (enableRemoteInitTimeline) {
+      // expect that initialization flow is called twice
+      when(timelineServiceClient.makeRequest(captor.capture()))
+          .thenReturn(wrapIntoRespose("timeline-hash"))
+          .thenReturn(wrapIntoRespose("true"))
+          .thenReturn(wrapIntoRespose("timeline-hash-2"))
+          .thenReturn(wrapIntoRespose("true"));
+    } else {
+      // otherwise, only one call is made to clear the current state in the server
+      when(timelineServiceClient.makeRequest(captor.capture())).thenReturn(wrapIntoRespose("true"));
+    }
+    RemoteHoodieTableFileSystemView view = new RemoteHoodieTableFileSystemView(metaClient, metaClient.getActiveTimeline(), timelineServiceClient, config);
+    String instantTime2 = "2";
+    HoodieInstant instant2 = new HoodieInstant(HoodieInstant.State.INFLIGHT, HoodieTimeline.COMMIT_ACTION, instantTime2, InstantComparatorV2.REQUESTED_TIME_BASED_COMPARATOR);
+    saveAsComplete(commitTimeline, instant2, new HoodieCommitMetadata());
+    view.sync();
+    List<TimelineServiceClientBase.Request> requests = captor.getAllValues().stream().filter(Objects::nonNull).collect(Collectors.toList());
+    if (enableRemoteInitTimeline) {
+      assertEquals(4, requests.size());
+      assertEquals(GET_TIMELINE_HASH_URL, requests.get(2).getPath());
+      assertEquals(INIT_TIMELINE_URL, requests.get(3).getPath());
+      // assert latest timeline is passed to server
+      metaClient.reloadActiveTimeline();
+      assertEquals(OBJECT_MAPPER.writeValueAsString(TimelineDTO.fromTimeline(metaClient.getActiveTimeline())), requests.get(3).getBody());
+    } else {
+      assertEquals(1, requests.size());
+      TimelineServiceClientBase.Request refreshRequest = requests.get(0);
+      assertEquals(TimelineServiceClientBase.RequestMethod.POST, refreshRequest.getMethod());
+      assertEquals(REFRESH_TABLE_URL, refreshRequest.getPath());
+    }
+  }
+
+  @Test
+  void closeRemoteView() throws Exception {
+    FileSystemViewStorageConfig config = FileSystemViewStorageConfig.newBuilder().build();
+    RemoteHoodieTableFileSystemView view = new RemoteHoodieTableFileSystemView(metaClient, metaClient.getActiveTimeline(), timelineServiceClient, config);
+    when(timelineServiceClient.makeRequest(any())).thenReturn(wrapIntoRespose("true"));
+    view.close();
+    // calling a second time should not make a second request
+    view.close();
+    // verify request parameters
+    verify(timelineServiceClient).makeRequest(argThat(request -> request.getMethod().equals(TimelineServiceClientBase.RequestMethod.POST) && request.getPath().equals(CLOSE_TABLE_URL)));
+  }
+
+  private TimelineServiceClientBase.Response wrapIntoRespose(String response) {
+    try {
+      return new TimelineServiceClientBase.Response(
+          new ByteArrayInputStream(OBJECT_MAPPER.writeValueAsString(response).getBytes(StandardCharsets.UTF_8)));
+    } catch (JsonProcessingException e) {
+      throw new HoodieException("Cannot create the correct response", e);
+    }
   }
 }
