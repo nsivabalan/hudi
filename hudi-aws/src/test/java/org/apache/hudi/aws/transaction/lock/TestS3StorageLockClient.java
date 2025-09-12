3dbb3930a4df3be39127feabecc3280a0cd50b1b
Alex R <alex@onehouse.ai>
2025-09-12 10:39:22 -0700

    [ENG-32150][HUDI-9782] Add audit service implementation for storage based lock provider (#1567)

--- hudi-aws/src/test/java/org/apache/hudi/aws/transaction/lock/TestS3StorageLockClient.java
+++ hudi-aws/src/test/java/org/apache/hudi/aws/transaction/lock/TestS3StorageLockClient.java
@@ -143,7 +143,7 @@ class TestS3StorageLockClient {
             (a,b) -> mockS3Client,
             mockLogger
     ));
-    assertTrue(ex.getMessage().contains("lock file path"));
+    assertTrue(ex.getMessage().contains("path"));
   }
 
   @Test
