0efdcdc5b1cea4c239c039c794a0e79fae4e46cf
Shubham Patel <42791613+shubhampatel28@users.noreply.github.com>
2025-12-12 11:21:49 -0800

    [ENG-34833] chore: Add SamplingLogger utility for reducing log volume while maintaining observability (#1637)

--- hudi-common/src/main/java/org/apache/hudi/common/table/view/AbstractTableFileSystemView.java
+++ hudi-common/src/main/java/org/apache/hudi/common/table/view/AbstractTableFileSystemView.java
@@ -36,6 +36,7 @@ import org.apache.hudi.common.util.ClusteringUtils;
 import org.apache.hudi.common.util.CompactionUtils;
 import org.apache.hudi.common.util.HoodieTimer;
 import org.apache.hudi.common.util.Option;
+import org.apache.hudi.common.util.SamplingLogger;
 import org.apache.hudi.common.util.ValidationUtils;
 import org.apache.hudi.common.util.collection.Pair;
 import org.apache.hudi.exception.HoodieException;
@@ -98,6 +99,9 @@ public abstract class AbstractTableFileSystemView implements SyncableFileSystemV
 
   // Used to concurrently load and populate partition views
   private final ConcurrentHashMap<String, Boolean> addedPartitions = new ConcurrentHashMap<>(4096);
+
+  // Sampling logger for replaced file groups read logs (log at INFO once every 5 times)
+  private final SamplingLogger replacedFileGroupsReadSamplingLogger = new SamplingLogger(LOG, 5);
 
   // Locks to control concurrency. Sync operations use write-lock blocking all fetch operations.
   // For the common-case, we allow concurrent read of single or multiple partitions
@@ -260,8 +264,10 @@ public abstract class AbstractTableFileSystemView implements SyncableFileSystemV
     Map<HoodieFileGroupId, HoodieInstant> replacedFileGroups = resultStream.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
         (instance1, instance2) -> HoodieTimeline.compareTimestamps(instance1.getTimestamp(), HoodieTimeline.LESSER_THAN, instance2.getTimestamp()) ? instance2 : instance1));
     resetReplacedFileGroups(replacedFileGroups);
-    LOG.info("Took " + hoodieTimer.endTimer() + " ms to read  " + replacedTimeline.countInstants() + " instants, "
-        + replacedFileGroups.size() + " replaced file groups");
+    // Sample log: log at INFO once every 5 times to track latencies, otherwise DEBUG
+    replacedFileGroupsReadSamplingLogger.logInfoOrDebug(
+        "Took {} ms to read {} instants, {} replaced file groups",
+        () -> new Object[]{hoodieTimer.endTimer(), replacedTimeline.countInstants(), replacedFileGroups.size()});
   }
 
   @Override
@@ -349,7 +355,11 @@ public abstract class AbstractTableFileSystemView implements SyncableFileSystemV
         long beginTs = System.currentTimeMillis();
         // Not loaded yet
         try {
-          LOG.debug("Building file system view for partitions: " + partitionSet);
+          if (metaClient.isMetadataTable()) {
+            LOG.debug("Building file system view for {} partition(s)", partitionSet.size());
+          } else {
+            LOG.info("Building file system view for {} partition(s)", partitionSet.size());
+          }
 
           // Pairs of relative partition path and absolute partition path
           List<Pair<String, Path>> absolutePartitionPathList = partitionSet.stream()
@@ -410,7 +420,11 @@ public abstract class AbstractTableFileSystemView implements SyncableFileSystemV
       if (!isPartitionAvailableInStore(partitionPathStr)) {
         // Not loaded yet
         try {
-          LOG.info("Building file system view for partition (" + partitionPathStr + ")");
+          if (metaClient.isMetadataTable()) {
+            LOG.debug("Building file system view for partition ({})", partitionPathStr);
+          } else {
+            LOG.info("Building file system view for partition ({})", partitionPathStr);
+          }
           List<HoodieFileGroup> groups = addFilesToView(partitionPathStr, getAllFilesInPartition(partitionPathStr));
           if (groups.isEmpty()) {
             storePartitionView(partitionPathStr, new ArrayList<>());
