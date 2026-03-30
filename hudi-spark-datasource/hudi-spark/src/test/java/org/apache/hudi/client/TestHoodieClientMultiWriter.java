3270eb5c78b9a8e89e9b9d6732162bd1a5c0a18d
vamsikarnika <vamsikarnika@gmail.com>
2026-03-30 19:18:37 +0530

    [ENG-38826][CHERRYPICK] fix: Fix test flakiness (#1797)
    Co-authored-by: Y Ethan Guo <ethan@onehouse.ai>

--- hudi-spark-datasource/hudi-spark/src/test/java/org/apache/hudi/client/TestHoodieClientMultiWriter.java
+++ hudi-spark-datasource/hudi-spark/src/test/java/org/apache/hudi/client/TestHoodieClientMultiWriter.java
@@ -722,6 +722,8 @@ public class TestHoodieClientMultiWriter extends HoodieClientTestBase {
           throw new RuntimeException(e);
         }
       }));
+      // Add small wait time to avoid flakiness
+      Thread.sleep(100);
     }
 
     futures.forEach(f -> {
